a5a4bfcf2fbe51961e709cbdf7a7a5435ceb343a
shivangi.maheshwari <shivangi.maheshwari@sciencelogic.com>
2020-01-24 13:32:07 +0530

    login logout functionality

---
+++ test_login.py
@@ -0,0 +1,31 @@
+
+from selenium import webdriver
+import time
+import unittest
+
+
+class loginTest(unittest.TestCase):
+
+    @classmethod  # using class methods of unittest class#
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="/home/shivangi/demo_project/chromedriver")
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+    def test_login_valid(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+        time.sleep(5)
+        self.driver.find_element_by_id("welcome").click()
+        self.driver.find_element_by_link_text("Logout").click()
+        time.sleep((2))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("test compeleted")
+
+
